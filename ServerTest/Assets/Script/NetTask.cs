ad594365ce29ec71daf3973651c531dffdada182
村田　崚 <eru1008ryo@gmail.com>
2019-02-25 10:59:41 +0900

    初期段階　ネットにつながっているかどうかの判定

---
+++ ServerTest/Assets/Script/NetTask.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NetTask : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Utility.IsTestButton())
+        {
+            Debug.Log(IsInternet());
+        }
+    }
+
+    //インターネットにつながっているかどうか
+    public static bool IsInternet()
+    {
+        //                                           繋がっていない場合↓
+        return !(Application.internetReachability == NetworkReachability.NotReachable);
+    }
+}
